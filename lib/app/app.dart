59847197dedc58de6a1e68403d000de3021a19e0
sadiascode <rahmanrohasadia@gmail.com>
2026-07-18 16:20:27 +0600

    working at app folder

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../featurs/auth/screen/login_screen.dart';
+
+
+class TechZone extends StatelessWidget {
+  const TechZone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tech Zone",
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
